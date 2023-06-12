55c3eb702647cfaae2ac138458f7db15d769c465
Sergeev Ilya <mailforstudyprog@yandex.ru>
2023-06-12 20:59:40 +0300

    added brain-games

--- src/Engine.php
+++ src/Engine.php
@@ -4,15 +4,19 @@ namespace BrainGamesPhp\Engine;
 
 use function BrainGamesPhp\Games\BrainEven\startEvenGame;
 use function BrainGamesPhp\Games\BrainCalc\startCalcGame;
+use function BrainGamesPhp\Games\BrainGames\startBrainGames;
 use function BrainGamesPhp\Games\BrainGcd\startGcdGame;
 use function BrainGamesPhp\Games\BrainPrime\startPrimeGame;
 use function BrainGamesPhp\Games\BrainPrime\startProgressionGame;
+
 use function cli\line;
 use function cli\prompt;
 
 function runGame($gameName)
 {
-    if ($gameName == 'brain-even') {
+    if ($gameName == 'brain-games') {
+        startBrainGames();
+    } elseif ($gameName == 'brain-even') {
         startEvenGame();
     } elseif ($gameName == 'brain-calc') {
         startCalcGame();
---
+++ src/Games/BrainGames.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BrainGamesPhp\Games\BrainGames;
+
+use function BrainGamesPhp\Cli\greeting;
+
+function startBrainGames()
+{
+    greeting();
+}
